5d8c108ba84e622519428f7e7e525f78da83e3c2
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-02 02:54:55 +0500

    1 module, 5 task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,9 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  let myStr = str;
+  if (str.length > maxlength) {
+    myStr = str.slice(0, maxlength - 1) + "…";
+  }
+  return myStr;
 }
