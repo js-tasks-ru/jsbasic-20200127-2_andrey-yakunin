ab8c815814e4fde40511261da8580e6549b939a8
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-05 01:07:30 +0500

    2 module, 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+  let answer = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] == 'number') {
+      answer += salaries[key];
+    }
+  }
+  return answer;
 }
