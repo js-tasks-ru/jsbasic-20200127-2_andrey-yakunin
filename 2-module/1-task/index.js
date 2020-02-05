32d1b6d406162fffaeaaabe51a9fbfbd1443e8e3
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-05 09:03:37 +0500

    update 2 module, 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,11 +3,23 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+// function sumSalary(salaries) {
+//   let answer = 0;
+//   for (let key in salaries) {
+//     if (typeof salaries[key] == 'number') {
+//       answer += salaries[key];
+//     }
+//   }
+//   return answer;
+// }
+
 function sumSalary(salaries) {
   let answer = 0;
   for (let key in salaries) {
-    if (typeof salaries[key] == 'number') {
-      answer += salaries[key];
+    if (salaries.hasOwnProperty(key)) {
+      if (typeof salaries[key] == 'number') {
+        answer += salaries[key];
+      }
     }
   }
   return answer;
