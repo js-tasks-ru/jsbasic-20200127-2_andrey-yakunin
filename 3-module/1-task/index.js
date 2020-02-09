93d6b4620a820cc8c1c51ec56f9a0359a0a97d52
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-10 01:18:33 +0500

    3 module, 1 task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,14 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let res = data
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`);
+  answer = res[0];
+  if (res.length > 1) {
+    for (item of res.slice(1)) {
+      answer += ('\n' + item);
+    }
+  }
+  return answer;
 }
