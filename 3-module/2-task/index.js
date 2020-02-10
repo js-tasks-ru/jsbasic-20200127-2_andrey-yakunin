61d84983d42a1857e3b75d4de2c1bf7449f1c6bf
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-10 21:45:06 +0500

    3 module, 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,15 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  nums = [];
+  for (let elem of str.split(/[, ]+/g)) {
+    if (isFinite(elem)) {
+      nums.push(elem);
+    }
+  }
+  nums = nums.sort(function (a, b) {return a - b});
+  return {
+    'min': +nums[0],
+    'max': +nums[nums.length - 1]
+  };
 }
