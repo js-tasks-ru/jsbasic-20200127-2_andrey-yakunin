1445ad2ef00bbfaa058f3888b6ccebf96bb2f7de
Andrey Yakunin <andrey.yakunin@gmail.com>
2020-02-17 00:46:16 +0500

    4 module, 1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    let li = document.createElement('li');
+    li.textContent = friend.firstName + ' ' + friend.lastName;
+    ul.append(li);
+  }
+  return ul;
 }
